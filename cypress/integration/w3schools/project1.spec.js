6bf2727054dd430afac7d954f3f98b848870e6d9
ataman-v-massmediagroup <mmg.ataman.v@gmail.com>
2021-05-11 15:29:37 +0300

    add new tests

--- cypress/integration/w3schools/project1.spec.js
+++ cypress/integration/w3schools/project1.spec.js
@@ -1,76 +1,94 @@
 import {
   checkH2Block,
-  textFirstAccordion,
+  firstPhotoCheck,
+  photoAttributesCheck,
+  clickArrowButtonNext,
+  secondPhotoCheck,
+  clickArrowButtonPrev,
+  loginFormButton,
+  checkModalWindow,
+  enterName,
+  enterPassword,
+  checkboxCheckOut,
+  pressLogInButton,
+  modalWindowInvisible,
+  checkTitleText,
+  firstSection,
+  quantityOfAccordions,
+  classOfOpenedAccordion,
+  closeAccordionCheckClass,
+  h2BlockName,
+  checkDropdownButton,
+  checkContent,
+  checkAndClickButton,
+  checkClassOfOpenedDropdown,
+  checkValueOfItemsInDropdown,
+  clickButtonAgain,
+  checkClassOfClosedDropdown,
+  clickAndCheckButton,
+  checkSideNavigationCanvasVisibility,
+  checkSideNavigationCanvasItems,
+  checkCloseButtonOfSideNavigationCanvas,
+  checkVisibilityOfClosedSideNavigationCanvas,
 } from "/home/dev/Project1/cypress/support/w3schools/project1.js";
 
 describe("Check in", function () {
   beforeEach(() => {
-    cy.visit("https://www.w3schools.com/howto/default_page1.asp");
+    cy.visit("/");
   });
   it("Check h2 block name", function () {
-    cy.get(".howtocontainer > div > :nth-of-type(1) > h2").should(
-      "have.text",
-      "Slideshow"
-    );
+    h2BlockName();
   });
 
   it("Check count photo", function () {
     checkH2Block("Slideshow");
-
-    // check count picture
-    cy.get(".numbertext:eq(0)").should("have.text", "1 / 4");
-    cy.get(".slfade").find("img").should("have.attr", "src");
-
-    // second picture count
-    cy.get(".slnext").click();
-    cy.get(".numbertext:eq(1)").should("have.text", "2 / 4");
-
-    cy.get(".slfade").find("img").should("have.attr", "src");
-    // check picture count when go back
-    cy.get(".slprev").click();
-    cy.get(".numbertext:eq(0)").should("have.text", "1 / 4");
-    cy.get(".slfade").find("img").should("have.attr", "src");
+    firstPhotoCheck();
+    photoAttributesCheck();
+    clickArrowButtonNext();
+    secondPhotoCheck();
+    photoAttributesCheck();
+    clickArrowButtonPrev();
+    firstPhotoCheck();
+    photoAttributesCheck();
   });
 
   it("Check Login Form", function () {
     checkH2Block("Login Form");
-    // going to login form
-    cy.get("button.w3-blue").should("contain", "Login").click();
-
-    cy.get(".w3-animate-zoom").should("be.visible");
-    // fill in name and password
-    cy.get("input[placeholder='Enter Username']")
-      .type("Ivan")
-      .should("have.value", "Ivan");
-    cy.get("input[placeholder='Enter Password']")
-      .type("Automation228")
-      .should("have.value", "Automation228");
-    // checking checkbox
-    cy.get("input[type='checkbox']")
-      .should("be.checked")
-      .uncheck()
-      .should("not.be.checked");
-    // finish login
-    cy.get("button.w3-section.w3-button")
-      .should("not.be.disabled")
-      .should("have.text", "Login")
-      .click();
-    cy.get("#loginModal").should("not.be.visible");
+    loginFormButton();
+    checkModalWindow();
+    enterName();
+    enterPassword();
+    checkboxCheckOut();
+    pressLogInButton();
+    modalWindowInvisible();
   });
-
   it("Check Accordion", function () {
     checkH2Block("Accordion");
-    // check title text
-    cy.get(".w3-container .h2-anchor:eq(2)").should("have.text", "Accordion");
-    //check text in 1-st section
-    cy.get(".showpanel > p").should("contain.text", textFirstAccordion);
-    // check quantity of accordions
-    cy.get(".howtocontainer > div > div:nth-of-type(3) > .w3-container")
-      .find(".accordion")
-      .should("have.length", 3);
-    // check class of opened accordion
-    cy.get("#myfirstAcc").should("have.class", "accordion activeacc");
-    // closed accordion and check its class
-    cy.get("#myfirstAcc").click().should("have.class", "accordion");
+    checkTitleText();
+    firstSection();
+    quantityOfAccordions();
+    classOfOpenedAccordion();
+    closeAccordionCheckClass();
+  });
+  it("Hover Dropdownss", function () {
+    checkH2Block("Hover Dropdowns");
+    checkDropdownButton();
+    checkContent();
+  });
+  it("Click Dropdowns", function () {
+    checkH2Block("Click Dropdowns");
+    checkAndClickButton();
+    checkClassOfOpenedDropdown();
+    checkValueOfItemsInDropdown();
+    clickButtonAgain();
+    checkClassOfClosedDropdown();
+  });
+  it("Side Navigation", function () {
+    checkH2Block("Side Navigation");
+    clickAndCheckButton();
+    checkSideNavigationCanvasVisibility();
+    checkSideNavigationCanvasItems();
+    checkCloseButtonOfSideNavigationCanvas();
+    checkVisibilityOfClosedSideNavigationCanvas();
   });
 });
--- cypress/support/w3schools/project1.js
+++ cypress/support/w3schools/project1.js
@@ -4,3 +4,180 @@ export function checkH2Block(nameH2) {
 
 export const textFirstAccordion =
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+export function firstPhotoCheck() {
+  const firstPhoto = ".numbertext:eq(0)";
+
+  cy.get(firstPhoto).should("have.text", "1 / 4");
+}
+
+export function photoAttributesCheck() {
+  const photoAttributes = ".slfade";
+
+  cy.get(photoAttributes).find("img").should("have.attr", "src");
+}
+
+export function clickArrowButtonNext() {
+  const ArrowButtonNext = ".slnext";
+
+  cy.get(ArrowButtonNext).click();
+}
+
+export function secondPhotoCheck() {
+  const secondPhoto = ".numbertext:eq(1)";
+
+  cy.get(secondPhoto).should("have.text", "2 / 4");
+}
+
+export function clickArrowButtonPrev() {
+  const arrowButtonPrev = ".slprev";
+
+  cy.get(arrowButtonPrev).click();
+}
+
+export function loginFormButton() {
+  const clickOnLoginFormButton = "button.w3-blue";
+
+  cy.get(clickOnLoginFormButton).should("contain", "Login").click();
+}
+
+export function checkModalWindow() {
+  const modalWindowIsVisible = ".w3-animate-zoom";
+
+  cy.get(modalWindowIsVisible).should("be.visible");
+}
+
+export function enterName() {
+  const enterNameInput = "input[placeholder='Enter Username']";
+
+  cy.get(enterNameInput).type("Ivan").should("have.value", "Ivan");
+}
+
+export function enterPassword() {
+  const enterPasswordInput = "input[placeholder='Enter Password']";
+
+  cy.get(enterPasswordInput)
+    .type("Automation228")
+    .should("have.value", "Automation228");
+}
+
+export function checkboxCheckOut() {
+  const checkUncheckBox = "input[type='checkbox']";
+
+  cy.get(checkUncheckBox)
+    .should("be.checked")
+    .uncheck()
+    .should("not.be.checked");
+}
+
+export function pressLogInButton() {
+  const pressingLogInButton = "button.w3-section.w3-button";
+
+  cy.get(pressingLogInButton)
+    .should("not.be.disabled")
+    .should("have.text", "Login")
+    .click();
+}
+
+export function modalWindowInvisible () {
+  const modalWindowinvisible = "#loginModal";
+
+  cy.get(modalWindowinvisible).should("not.be.visible");
+}
+
+export function checkTitleText() {
+  const checkOutTitleText = ".w3-container .h2-anchor:eq(2)";
+
+  cy.get(checkOutTitleText).should("have.text", "Accordion");
+}
+
+export function firstSection() {
+  const checkFirstSection = ".showpanel > p";
+
+  cy.get(checkFirstSection).should("contain.text", textFirstAccordion);
+}
+
+export function quantityOfAccordions() {
+  const howMuchAccordions =
+    ".howtocontainer > div > div:nth-of-type(3) > .w3-container";
+
+  cy.get(howMuchAccordions).find(".accordion").should("have.length", 3);
+}
+
+export function classOfOpenedAccordion() {
+  const accordionsClass = "#myfirstAcc";
+
+  cy.get(accordionsClass).should("have.class", "accordion activeacc");
+}
+
+export function closeAccordionCheckClass() {
+  const classOfClosedAccordion = "#myfirstAcc";
+
+  cy.get(classOfClosedAccordion).click().should("have.class", "accordion");
+}
+
+export function h2BlockName() {
+  const nameOfH2Block = ".howtocontainer > div > :nth-of-type(1) > h2";
+
+  cy.get(nameOfH2Block).should("have.text", "Slideshow");
+}
+
+export function checkDropdownButton() {
+  const checkDropDownButton = ".dropdown2 > .dropbtn";
+  cy.get(checkDropDownButton).should("have.text", "Hover");
+}
+
+export function checkContent() {
+  const contentCheck = ".dropdown2 > .dropdown-content > a";
+  cy.get(contentCheck).should("have.length", 3);
+}
+
+export function checkAndClickButton() {
+  const CheckClick = "[onclick='myFunction()']";
+  cy.get(CheckClick).should("have.text", "Click").click();
+}
+
+export function checkClassOfOpenedDropdown() {
+  const openedDropDownClass = "#myDropdown";
+  cy.get(openedDropDownClass).should("have.class", "dropdown-content show");
+}
+
+export function checkValueOfItemsInDropdown() {
+  const valueItemsInDropdown = "#myDropdown >a";
+  cy.get(valueItemsInDropdown).should("have.length", 3);
+}
+
+export function clickButtonAgain() {
+  const AgainButton = "[onclick='myFunction()']";
+  cy.get(AgainButton).click();
+}
+
+export function checkClassOfClosedDropdown() {
+  const classOfClosedDropdown = "#myDropdown";
+  cy.get(classOfClosedDropdown).should("have.class", "dropdown-content");
+}
+
+export function clickAndCheckButton() {
+  const buttonCheckClick = "[onclick='openOffcanvas1()']";
+  cy.get(buttonCheckClick).should("have.text", "Sidenav overlay").click();
+}
+
+export function checkSideNavigationCanvasVisibility() {
+  const canvasVisibility = "#myOffcanvas";
+  cy.get(canvasVisibility).should("be.visible");
+}
+
+export function checkSideNavigationCanvasItems() {
+  const canvasItems = "#myOffcanvas a";
+  cy.get(canvasItems).should("have.length", 5);
+}
+
+export function checkCloseButtonOfSideNavigationCanvas() {
+  const canvasCloseButton = ".closeOffcanvas";
+  cy.get(canvasCloseButton).click();
+}
+
+export function checkVisibilityOfClosedSideNavigationCanvas () {
+  const visibilityOfClosedCanvas = "#myOffcanvas";
+  cy.get(visibilityOfClosedCanvas).should("not.be.visible");
+}
